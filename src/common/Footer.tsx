57788289d30edb0cf64ba259cafef64d153f5570
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-07-18 16:36:14 +0530

    Make footer responsive

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -17,8 +17,8 @@ const socials = [
 export default function Footer() {
   return (
     <section className="p-page flex py-12 overflow-hidden relative mt-5 widescreen:flex-col-reverse">
-      {/* <div className="absolute top-0 left-0 w-[70%] bg-secondary -translate-x-[30%] rounded-full aspect-square -z-1" /> */}
-      <div className="basis-1/2 flex flex-col gap-y-6 text-back">
+      <div className="absolute top-0 left-0 w-[70%] bg-secondary -translate-x-[30%] rounded-full aspect-square -z-1 widescreen:mt-96 widescreen:w-[150vw] widescreen:h-128" />
+      <div className="basis-1/2 flex flex-col gap-y-6 text-back widescreen:mt-32">
         <Link to="/" className="text-3xl font-bold w-max">
           Karrom
         </Link>
@@ -41,18 +41,18 @@ export default function Footer() {
         <div className="flex gap-x-6 my-3">
           <Link
             to="wa.me/8888888888"
-            className="flex items-center bg-[#15b346] text-white px-8 py-3 text-lg rounded-md"
+            className="flex items-center bg-[#15b346] text-white px-8 py-3 text-lg rounded-md mobile:px-2 mobile:text-sm"
           >
             <img
               src="https://seeklogo.com/images/W/whatsapp-logo-112413FAA7-seeklogo.com.png"
               alt="Whatsapp"
-              className="w-[1em] mr-2 aspect-square brightness-0 invert"
+              className="w-[1em] mr-2 aspect-square brightness-0 invert "
             />
             Message Us
           </Link>
           <Link
             to="mailto:support@karrom.edu"
-            className="flex items-center border-front border-2 border-opacity-20 duration-300 hover:border-opacity-100 px-8 py-3 text-lg rounded-md"
+            className="flex items-center border-front border-2 border-opacity-20 duration-300 hover:border-opacity-100 px-8 py-3 text-lg rounded-md mobile:px-2 mobile:text-sm"
           >
             <MaterialIcon codepoint="e158" className="mr-2" />
             support@karrom.edu
