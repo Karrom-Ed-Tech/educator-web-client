ce0ee538719b5708c9f65743f28236cd5db389eb
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-07-20 16:31:11 +0530

    Add profile hero section

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,14 @@ import HomePage from "./pages/HomePage/HomePage";
 import { GlobalContextProvider } from "./contexts/globalContext";
 import ScrollToTop from "./common/ScrollToTop";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Route>
     )
   );
---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+
+export default function ProfilePage() {
+    return (
+        <div>
+        <h1>
+            <Hero/>
+        </h1>
+        </div>
+    );
+}
